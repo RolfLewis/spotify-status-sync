4c934ef7ea7ae5703e1c6d4972cc54be2bcc5e93
Rolf Lewis <rlewis@grax.com>
2021-04-17 08:55:39 -0400

    testing full spotify auth flow

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -9,6 +13,18 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
+
+var appURL = "https://spotify-status-sync.herokuapp.com/"
+var spotifyBaseURL = "https://accounts.spotify.com/"
+var spotifyClient *http.Client
+
+type spotifyAuthResponse struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	Scope        string `json:"scope"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -29,13 +45,28 @@ func main() {
 	router.GET("/login", loginFlow)
 	router.GET("/callback", callbackFlow)
 
+	// Create the global spotify client
+	spotifyClient = http.DefaultClient
+
 	router.Run(":" + port)
 }
 
+func getLoginRedirectURL() string {
+	return appURL + "callback"
+}
+
 func loginFlow(context *gin.Context) {
-	clientID := os.Getenv("SPOTIFY_CLIENT_ID")
-	callbackURL := "https://spotify-status-sync.herokuapp.com/callback"
-	spotifyAuthURL := "https://accounts.spotify.com/authorize?client_id=" + clientID + "&response_type=code&redirect_uri=" + url.PathEscape(callbackURL) + "&scope=user-read-currently-playing"
+	callbackURL := getLoginRedirectURL()
+
+	// Set the query values
+	queryValues := url.Values{}
+	queryValues.Set("client_id", os.Getenv("SPOTIFY_CLIENT_ID"))
+	queryValues.Set("response_type", "code")
+	queryValues.Set("redirect_uri", url.PathEscape(callbackURL))
+	queryValues.Set("scope", "user-read-currently-playing")
+
+	// Redirect to spotify OAuth page
+	spotifyAuthURL := spotifyBaseURL + "/authorize?" + queryValues.Encode()
 	context.Redirect(http.StatusPermanentRedirect, spotifyAuthURL)
 }
 
@@ -45,7 +76,62 @@ func callbackFlow(context *gin.Context) {
 
 	if errorMsg != "" {
 		context.String(http.StatusInternalServerError, errorMsg)
-	} else {
-		context.String(http.StatusOK, code)
+		return
+	}
+
+	// Get the auth and refresh tokens
+	req, reqError := http.NewRequest(http.MethodPost, spotifyBaseURL+"api/token", nil)
+	if reqError != nil {
+		context.String(http.StatusInternalServerError, reqError.Error())
+		return
+	}
+
+	// Encode the authorization header
+	bytes := []byte(os.Getenv("SPOTIFY_CLIENT_ID") + ":" + os.Getenv("SPOTIFY_CLIENT_SECRET"))
+	req.Header.Add("Authorization", "Basic "+base64.StdEncoding.EncodeToString(bytes))
+
+	// Set the query values
+	queryValues := url.Values{}
+	queryValues.Set("grant_type", "authorization_code")
+	queryValues.Set("code", code)
+	queryValues.Set("redirect_uri", getLoginRedirectURL())
+
+	// Send the request
+	resp, respError := spotifyClient.Do(req)
+	if respError != nil {
+		context.String(http.StatusInternalServerError, respError.Error())
+		return
+	}
+
+	defer resp.Body.Close()
+
+	// Read the tokens
+	jsonBytes, readError := ioutil.ReadAll(resp.Body)
+	if readError != nil {
+		context.String(http.StatusInternalServerError, readError.Error())
+		return
+	}
+
+	var tokens spotifyAuthResponse
+	jsonError := json.Unmarshal(jsonBytes, &tokens)
+	if jsonError != nil {
+		context.String(http.StatusInternalServerError, jsonError.Error())
+		return
+	}
+
+	context.String(http.StatusOK, tokens.AccessToken)
+}
+
+func writeSpotifyToken(token string) {
+	tokenFile, fileError := os.Create("spotify-token")
+	if fileError != nil {
+		log.Fatal(fileError)
+	}
+	written, writeError := tokenFile.WriteString(token)
+	if writeError != nil {
+		log.Fatal(writeError)
+	}
+	if written != len(token) {
+		log.Fatal(errors.New("Full token not written"))
 	}
 }
