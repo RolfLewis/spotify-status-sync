65adaf46e465aa33e19d2e36eeabbd35e9531a1c
Rolf Lewis <rlewis@grax.com>
2021-04-17 22:20:08 -0400

    bug fix for null spotify_id in table select

--- schema.go
+++ schema.go
@@ -97,8 +97,8 @@ func addSpotifyToUser(user string, profile spotifyProfile, tokens spotifyAuthRes
 func getSpotifyForUser(user string) (string, *spotifyAuthResponse, error) {
 	// Get the spotify ID from the session
 	var spotifyID string
-	scanError := appDatabase.QueryRowx("SELECT spotify_id FROM slackaccounts WHERE id=$1", user).Scan(&spotifyID)
-	if scanError != nil && scanError == sql.ErrNoRows {
+	scanError := appDatabase.QueryRowx("SELECT spotify_id FROM slackaccounts WHERE id=$1 AND spotify_id NOT null", user).Scan(&spotifyID)
+	if scanError == sql.ErrNoRows {
 		return "", nil, nil // Nothing to return
 	} else if scanError != nil {
 		return "", nil, scanError
